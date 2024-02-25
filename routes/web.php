2c52f3dd0cd42ee3762d198fd1d25405b8e7f83e
Ikhsanfzn <ikhsan.fzn20@gmail.com>
2024-02-25 08:54:08 +0700

    upload tugas-6

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\PegawaiController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiController::class, 'tambah']);
+Route::get('/pegawai/edit/{id}', [PegawaiController::class, 'edit']);
+Route::get('/pegawai/hapus/{id}', [PegawaiController::class, 'hapus']);
+Route::post('/pegawai/update', [PegawaiController::class, 'update']);
+Route::post('/pegawai/store', [PegawaiController::class,'store']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
